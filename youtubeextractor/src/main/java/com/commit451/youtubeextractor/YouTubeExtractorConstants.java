d531388d2abf3c7557d5bfdd900ca05da017cd15
Teocci <jrfdeft@gmail.com>
2016-12-15 10:36:12 +0900

    Some changes in Ubuntu

--- youtubeextractor/src/main/java/com/commit451/youtubeextractor/YouTubeExtractorConstants.java
+++
@@ -1,8 +0,0 @@
-package com.commit451.youtubeextractor;
-
-public interface YouTubeExtractorConstants {
-
-    String INFO = "get_video_info?el=info&ps=default&gl=US";
-    String VIDEO_ID = "video_id";
-
-}
